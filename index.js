d2ccff37af98d061a8d91f83c19aea0487f95dfc
Allan5829 <allan.farca@gmail.com>
2020-09-23 08:56:58 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code here!
+document.querySelector('main#main').remove();
+let newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory");
+newHeader.innerHTML = 'is the champion'
