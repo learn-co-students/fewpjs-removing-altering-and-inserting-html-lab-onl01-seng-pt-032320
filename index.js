27fdf899187c2d464bb143aa9f6ac849af52e86e
vlad <VladDaImpaler@gmail.com>
2020-09-28 23:11:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+let thing = document.querySelector("main#main");
+thing.remove();
+let newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "VLADIMIR is the champion"
