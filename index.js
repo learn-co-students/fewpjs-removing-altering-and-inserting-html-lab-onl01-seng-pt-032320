27a8a7820fbdb6aa72a57b312e154163b0d0b626
AaronJHarvey <61170939+AaronJHarvey@users.noreply.github.com>
2020-09-29 20:40:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+
+
+document.querySelector("main#main").remove()// finds the element, in this case main) and removes it.
+
+
+const newHeader = document.createElement("h1") // sets the variable NewHeader equal to an new element on this document called "h1"
+
+newHeader.id = "victory" // refers to the variable set previously and makes its id "victory"
+newHeader.innerHTML = "Flatiron School is the champion!" //makes the h1 element read "Flatiron is the champion!"
