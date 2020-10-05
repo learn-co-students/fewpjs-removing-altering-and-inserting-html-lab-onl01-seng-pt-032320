b1d22e2221d0a3476da8ea195271932a3470782c
tiabeanie <sydney.jane.hunter@gmail.com>
2020-10-05 09:33:24 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+document.querySelector('main').remove();
+
+let newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerHTML = "Sydney is the champion";
+document.body.appendChild(newHeader);
